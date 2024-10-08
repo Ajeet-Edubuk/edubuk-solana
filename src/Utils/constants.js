1de851ac93463fb05daaba1545eb41a23b48067c
ajeetram <86039147+ajeetram@users.noreply.github.com>
2024-10-08 22:12:29 +0530

    redployed on solana

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -2,7 +2,7 @@ import { PublicKey } from '@solana/web3.js';
 import idl from '../constant/idl.json'; 
 
 // Program ID from Rust code
-export const PROGRAM_ID = new PublicKey("5qLv4CJ4fhy5QoPTUsQhhQNJRDBNX9mv5iMyLtv16jag");
+export const PROGRAM_ID = new PublicKey("HH18xCQFFpoMscFJdTQEoRHqpmnUbb2Jxnr2BWbBqhCD");
 
 // idl.json file 
 export const IDL = idl;
